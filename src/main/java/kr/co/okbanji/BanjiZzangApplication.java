65f7a406ba79d084fb72f8c7059c6b2c0b9ce8e5
okbanji <eunashine@gmail.com>
2020-11-17 21:26:07 +0900

    first commit

---
+++ src/main/java/kr/co/okbanji/BanjiZzangApplication.java
@@ -0,0 +1,13 @@
+package kr.co.okbanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanjiZzangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanjiZzangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/okbanji/BanjiZzangApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.okbanji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanjiZzangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
